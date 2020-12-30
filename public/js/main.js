4437e825a5f7a09dbbf061c44e11725495f0a195
Achraf <hardy1348@live.fr>
2020-12-30 14:51:58 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+let test = ["code", "code1", "code3"];
+test.forEach(element => console.log(element));
+
+let animal = test.push("Achraf")
+console.log(test);
+test.forEach(element => console.log(element));
+
+
